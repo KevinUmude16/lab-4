7e786ae8162d33ae6579adbad7a9e1e8def0140a
KevinUmude16 <kevosky16.ku@gmail.com>
2024-11-18 15:13:35 +0000

    commits

--- src/Components/movieitem.js
+++ src/Components/movieitem.js
@@ -1,25 +1,39 @@
-// Define the MovieItem functional component
-import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
-import { useEffect } from 'react';
-const MovieItem = (props) =>{
-    // Run the movie item
-useEffect(() =>{
-    console.log("Movie Item:", props.mymovie);
-}, [props.mymovie]);
+// Import necessary dependencies from React, React Router, and React Bootstrap
+import { Link } from 'react-router-dom'; // Link is used to navigate between routes without reloading the page
+import { Card } from 'react-bootstrap'; // Card is a component from React Bootstrap for styling
+import { useEffect } from 'react'; // useEffect is used to perform side effects (e.g., logging)
 
-    return(
+const MovieItem = (props) => {
+    // useEffect hook to log the current movie data whenever `props.mymovie` changes
+    useEffect(() => {
+        // Log the `mymovie` prop to the console (this can be useful for debugging)
+        console.log("Movie Item:", props.mymovie);
+    }, [props.mymovie]); // The effect runs when the `mymovie` prop changes
+
+    return (
         <div>
+            {/* Card component used to display the movie details */}
             <Card>
-            <h2>{props.myMovie.title}</h2>
-            <p>{props.myMovie.year}</p>
-            <img src ={props.myMovie.poster}></img>
-            <Card.Body>
-            <Link className="btn btn-primary" to={"/edit/" + props.myMovie._id} >Update</Link>
-            </Card.Body>
+                {/* Movie title displayed as a heading */}
+                <h2>{props.myMovie.title}</h2>
+                
+                {/* Movie release year */}
+                <p>{props.myMovie.year}</p>
+
+                {/* Movie poster image */}
+                <img src={props.myMovie.poster} alt={props.myMovie.title} />
+                
+                {/* Card body contains the update button */}
+                <Card.Body>
+                    {/* Link to navigate to the edit page for this specific movie */}
+                    <Link className="btn btn-primary" to={"/edit/" + props.myMovie._id} >
+                        Update
+                    </Link>
+                </Card.Body>
             </Card>
         </div>
     );
 }
+
 // Export the MovieItem component for use in other files
 export default MovieItem;
