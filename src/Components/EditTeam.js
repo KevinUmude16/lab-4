6ce8e895c4ba36a6cdf708c2954755295584ebf4
KevinUmude16 <kevosky16.ku@gmail.com>
2024-12-06 22:46:46 +0000

    commits

--- src/Components/EditTeam.js
+++ src/Components/EditTeam.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../App.css';
 
 const EditTeam = ({ team, onTeamUpdated }) => {
     const [newName, setNewName] = useState(team.name);
